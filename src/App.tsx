2e25a7c0b6a332135752e80fba35096bba535ca5
James Vickery <14852491+jmsv@users.noreply.github.com>
2025-06-04 21:03:22 +0100

    tw + shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <>hello</>;
+  return (
+    <div className="flex flex-col p-4 gap-2 items-center">
+      <p>hello</p>
+      <Button>button</Button>
+    </div>
+  );
 }
 
 export default App;
